95d5fcaa273458fc45c95d748e6df16d9e2e3d1b
ArK761 <peter.knotek76@gmail.com>
2026-09-24 20:11:56 +0200

    Add configuration download route

--- netbox_libreoxi/urls.py
+++ netbox_libreoxi/urls.py
@@ -6,4 +6,5 @@ app_name = "netbox_libreoxi"
 
 urlpatterns = [
     path("settings/", views.settings_view, name="settings"),
+    path("devices/<int:pk>/download/", views.download_config, name="download_config"),
 ]
