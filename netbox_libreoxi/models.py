94b8f3107ba457447f35844c314a2ec27dd2c406
ArK761 <peter.knotek76@gmail.com>
2026-09-24 22:23:38 +0200

    Add true cron schedule setting

--- netbox_libreoxi/models.py
+++ netbox_libreoxi/models.py
@@ -15,6 +15,7 @@ class LibreOXISettings(models.Model):
     )
     request_timeout = models.PositiveIntegerField(default=10)
     check_interval_minutes = models.PositiveIntegerField(default=5)
+    schedule_cron = models.TextField(default="*/5 * * * *")
     retention_days = models.PositiveIntegerField(default=365)
     retention_revisions = models.PositiveIntegerField(default=100)
     verify_tls = models.BooleanField(default=True)
