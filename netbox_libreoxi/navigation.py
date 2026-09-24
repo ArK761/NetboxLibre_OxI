da42edda2bba82c290ba622bb7b7b38040c100c0
ArK761 <peter.knotek76@gmail.com>
2026-09-24 10:54:50 +0200

    Add dedicated LibreOXI navigation menu

--- netbox_libreoxi/navigation.py
+++ netbox_libreoxi/navigation.py
@@ -1,9 +1,18 @@
-from netbox.plugins import PluginMenuItem
+from netbox.plugins import PluginMenu, PluginMenuItem
 
 
-menu_items = (
-    PluginMenuItem(
-        link="plugins:netbox_libreoxi:settings",
-        link_text="LibreOXI",
-    ),
+settings_item = PluginMenuItem(
+    link="plugins:netbox_libreoxi:settings",
+    link_text="Settings",
 )
+
+
+menu = PluginMenu(
+    label="LibreOXI",
+    groups=(
+        ("LibreOXI", (settings_item,)),
+    ),
+    icon_class="mdi mdi-content-save-cog",
+)
+
+menu_items = (settings_item,)
