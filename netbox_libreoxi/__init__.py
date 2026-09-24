3cbc7dfefbf6c43aa46f74a28930f79dab280293
ArK761 <peter.knotek76@gmail.com>
2026-09-24 07:21:02 +0200

    Add netbox_libreoxi/__init__.py

---
+++ netbox_libreoxi/__init__.py
@@ -0,0 +1,22 @@
+from netbox.plugins import PluginConfig
+
+
+class NetBoxLibreOXIConfig(PluginConfig):
+    name = "netbox_libreoxi"
+    verbose_name = "NetBox LibreOXI"
+    description = "Retrieve and track network device configurations from LibreNMS Oxidized/OXI."
+    version = "0.1.0"
+    base_url = "libreoxi"
+    min_version = "4.4.0"
+    max_version = "4.9.99"
+    default_settings = {
+        "storage_root": "/var/lib/netbox/libreoxi",
+        "request_timeout": 10,
+        "check_interval_minutes": 60,
+        "retention_days": 365,
+        "retention_revisions": 100,
+        "verify_tls": True,
+    }
+
+
+config = NetBoxLibreOXIConfig
