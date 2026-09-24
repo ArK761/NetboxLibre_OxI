9bdd6785682c990dca54498e0d22999e32286ee5
ArK761 <peter.knotek76@gmail.com>
2026-09-24 22:33:43 +0200

    Fix migration chain: add schedule_cron as 0005

---
+++ netbox_libreoxi/migrations/0005_schedule_cron.py
@@ -0,0 +1,32 @@
+from django.db import migrations, models
+
+
+def migrate_interval_to_cron(apps, schema_editor):
+    settings_model = apps.get_model("netbox_libreoxi", "LibreOXISettings")
+    mapping = {
+        5: "*/5 * * * *",
+        10: "*/10 * * * *",
+        15: "*/15 * * * *",
+        20: "*/20 * * * *",
+        30: "*/30 * * * *",
+        60: "0 * * * *",
+        90: "30 */1 * * *",
+    }
+    for settings in settings_model.objects.all():
+        settings.schedule_cron = mapping.get(settings.check_interval_minutes, "*/5 * * * *")
+        settings.save(update_fields=["schedule_cron"])
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("netbox_libreoxi", "0004_datetime_format"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="libreoxisettings",
+            name="schedule_cron",
+            field=models.TextField(default="*/5 * * * *"),
+        ),
+        migrations.RunPython(migrate_interval_to_cron, migrations.RunPython.noop),
+    ]
