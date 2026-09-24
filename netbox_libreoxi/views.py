d2318e15842425e43162416ec87bd62bd0e65b7a
ArK761 <peter.knotek76@gmail.com>
2026-09-24 11:01:00 +0200

    Add LibreOXI device object tab

--- netbox_libreoxi/views.py
+++ netbox_libreoxi/views.py
@@ -1,8 +1,30 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
 
+from dcim.models import Device
+from netbox.views import generic
+from utilities.views import ViewTab, register_model_view
+
 from .forms import LibreOXISettingsForm
 from .models import LibreOXISettings
+
+
+@register_model_view(Device, name="libreoxi", path="libreoxi")
+class DeviceLibreOXIView(generic.ObjectView):
+    queryset = Device.objects.all()
+    tab = ViewTab(label="LibreOXI", weight=500)
+
+    def get(self, request, pk):
+        device = self.get_object(request, pk)
+        return render(
+            request,
+            "netbox_libreoxi/device_tab.html",
+            {
+                "object": device,
+                "device": device,
+                "tab": self.tab,
+            },
+        )
 
 
 def settings_view(request):
